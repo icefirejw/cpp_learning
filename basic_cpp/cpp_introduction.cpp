35ecea1588aed23308f49364d3210288bf6f7012
Sam <samwang_cn@outlook.com>
2019-06-23 10:52:31 +0800

    变量

--- basic_cpp/cpp_introduction.cpp
+++ basic_cpp/cpp_introduction.cpp
@@ -11,12 +11,24 @@ using namespace std;
  * 这是多行注释
  * 变量的示意
  * */
-int variable()
+int Variable()
 {
     //单行注释： 定义变量a，初始化值是10
     //定义变量就是值分配一块内存，将这块内存的名字叫做这个变量
     int a = 10;
-    cout << "变量a的值是：" << a << endl;
+    cout << "the value of a is ：" << a << endl;
+
+    return 0;
+}
+
+int SizeOfFunc()
+{
+
+    cout << "size of char is :    " << sizeof(char) <<endl;
+    cout << "size of integer is : " << sizeof(int) << endl;
+    cout << "size of long is :    " << sizeof(long) << endl;
+    cout << "size of float is :   " << sizeof(float) << endl;
+    cout << "size of double is :  " << sizeof(double) <<endl;
 
     return 0;
 }
@@ -24,7 +36,9 @@ int variable()
 #ifdef BASIC_CPP_CPP_INTRODUCTION
 int main()
 {
-    variable();
+    Variable();
+    SizeOfFunc();
+    
     return 0;
 }
 #endif
